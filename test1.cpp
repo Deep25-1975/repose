6af0e64a6254d8b437af65154ce8ab7970d4b719
Deep25-1975 <142021151+Deep25-1975@users.noreply.github.com>
2024-11-11 15:39:40 +0530

    Update test1.cpp

--- test1.cpp
+++ test1.cpp
@@ -3,6 +3,6 @@
 int main() {
     std::cout << "Hello, World!" << std::endl;
     std::cout << "Version control"<< std::endl;
-    std::cout << " Git reository"<< std ::endl;
+    std::cout << " Git reository and versio control std ::endl;
     return 0;
 }
